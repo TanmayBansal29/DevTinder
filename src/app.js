fb16bd4d4fcbcf6adea8dafc520e2847fd8fcf8c
Tanmay Bansal <bansaltanmay554@gmail.com>
2025-04-10 23:49:45 +0530

    Explored the HTTP Methods

--- src/app.js
+++ src/app.js
@@ -3,12 +3,36 @@ const express = require("express")
 const app = express()
 const PORT = 3000
 
-app.use("/hello", (req, res) => {
-    res.send("Hello Hello Hello") // Request Handler Function
+// app.use("/hello", (req, res) => {
+//     res.send("Hello Hello Hello") // Request Handler Function
+// })
+
+// app.use("/test", (req, res) => {
+//     res.send("Hello from the server") // Request Handler Function
+// })
+
+// app.use("/", (req, res) => {
+//     res.send("Hnjiiii") // Request Handler Function
+// })
+
+// app.use("/user", (req,res) => {
+//     res.send("Aa gye phir se maut ka tamasha dekhne")
+// })
+
+app.get("/user", (req, res) => {
+    res.send({firstName: "Tanmay", lastName: "Bansal"})
 })
 
-app.use("/test", (req, res) => {
-    res.send("Hello from the server") // Request Handler Function
+app.post("/user", (req,res) => {
+    res.send("Data Successfully saved to Database")
+})
+
+app.delete("/user", (req,res) => {
+    res.send("Deleted Successfully")
+})
+
+app.put("/user", (req,res) => {
+    res.send("Data Updated")
 })
 
 app.listen(PORT, () => {
