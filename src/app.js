0685027371a9f52b127d868921eb17421e89663e
Tanmay Bansal <bansaltanmay554@gmail.com>
2025-04-18 00:02:18 +0530

    creating-the-mongoose-schema

--- src/app.js
+++ src/app.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const connectDB = require("./config/database")
-
+const User = require("./models/user")
 const app = express();
 const PORT = 3000;
+
+app.post("/signup", async (req,res) => {
+    const userObj = {
+        firstName: "Virat",
+        lastName: "Kohli",
+        emailId: "virat.kohli@gmail.com",
+        password: "virat@123"
+    }
+
+    // Creating a new instance of user model
+    const user = new User(userObj)
+    try {
+        await user.save();
+        res.send("User Added Successfully")
+    } catch (err) {
+        res.status(400).send("Error Saving the User: " + err.message)
+    }
+})
 
 // Resolving the promise returned using try catch
 connectDB().then(() => {
