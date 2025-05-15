6a5fe53fe325ad949e057eebaae3d22b028185b2
Tanmay Bansal <bansaltanmay554@gmail.com>
2025-05-15 13:53:20 +0530

    Connection review API created

--- src/routes/request.js
+++ src/routes/request.js
@@ -60,4 +60,45 @@ router.post("/request/send/:status/:toUserId", userAuth, async (req, res) => {
     }
 })
 
+router.post("/request/review/:status/:requestId", userAuth, async(req, res) => {
+    try {
+        const loggedInUser = req.user
+        const allowedStatus = ["accepted", "rejected"]
+        const status = req.params.status
+        const requestId = req.params.requestId
+
+        // If status is allowed or rejected for this API or some other
+        if(!allowedStatus.includes(status)){
+            return res.status(400).json({
+                message: "Invalid Status type: " + status
+            })
+        }
+
+        const connectionRequest = await ConnectionRequest.findOne({
+            _id: requestId,
+            toUserId: loggedInUser._id,
+            status: "interested"
+        })
+
+        if(!connectionRequest) {
+            return res.status(404).json({
+                message : "Connection Request Not Found"
+            })
+        }
+
+        connectionRequest.status = status;
+        await connectionRequest.save()
+        return res.status(200).json({
+            message: "Connection Request Accepted",
+            connectionRequest
+        })
+
+    } catch (err) {
+        return res.status(400).json({
+            message: "Error Occured",
+            error: err
+        })
+    }
+})
+
 module.exports = router
