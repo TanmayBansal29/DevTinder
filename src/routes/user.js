39660a7eeab14a85647baf491d6278e255e458aa
Tanmay Bansal <bansaltanmay554@gmail.com>
2025-05-15 15:42:03 +0530

    Solved the bug in connections API

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,8 @@ const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connection");
 const router = express.Router()
 
+const USER_SAFE_DATA = ["firstName", "lastName", "age", "gender", "photoUrl", "about", "skills"]
+
 // Get all the pending connection requests for the loggedIn User
 router.get("/user/requests/received", userAuth, async (req, res) => {
     try {
@@ -10,7 +12,7 @@ router.get("/user/requests/received", userAuth, async (req, res) => {
         const connectionRequests = await ConnectionRequest.find({
             toUserId: loggedInUser._id,
             status: 'interested'
-        }).populate("fromUserId", ["firstName", "lastName", "age", "gender", "photoUrl", "about", "skills"])
+        }).populate("fromUserId", USER_SAFE_DATA)
 
         return res.status(200).json({
             message: "Conenction Requests Fetched Successfully",
@@ -33,9 +35,17 @@ router.get("/user/connections", userAuth, async (req, res) => {
                 {toUserId: loggedInUser._id, status: "accepted"},
                 {fromUserId: loggedInUser._id, status: "accepted"}
             ],
-        }).populate("fromUserId", ["firstName", "lastName", "age", "gender", "photoUrl", "about", "skills"])
+        })
+        .populate("fromUserId", USER_SAFE_DATA)
+        .populate("toUserId", USER_SAFE_DATA)
 
-        const data = connectionRequests.map((row) => row.fromUserId);
+        const data = connectionRequests.map((row) => {
+            if(row.fromUserId._id.toString() === loggedInUser._id.toString()){
+                return row.toUserId
+            } else {
+                return row.fromUserId
+            }
+        });
 
         return res.status(200).json({
             message: "Connection Requests Found",
